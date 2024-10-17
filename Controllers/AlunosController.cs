1d29b6279069f5e0921eb680b088359e6bd72c5b
Nícolas Bassini <nicolasbassini@hotmail.com>
2024-10-17 13:09:21 -0300

    dia3

--- Controllers/AlunosController.cs
+++ Controllers/AlunosController.cs
@@ -32,4 +32,59 @@ public class AlunosController : ControllerBase
     {
         return Ok(await _alunoService.GetAlunos());
     }
+    [HttpGet("AlunoPorNome")]
+    public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunosByNome([FromQuery] string nome)
+    {
+        try
+        {
+            IEnumerable<Aluno> alunos = await _alunoService.GetAlunosByNome(nome);
+            if (alunos == null || alunos.Count() == 0)
+                return NotFound($"Não existem alunos com o critério {nome}.");
+            return Ok(alunos);
+        }
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao buscar alunos");
+        }
+    }
+
+    [HttpGet("{id:int}", Name = "GetAluno")]
+    public async Task<ActionResult<Aluno>> GetAluno(int id)
+    {
+        Aluno? aluno = await _alunoService.GetAluno(id);
+        if (aluno == null)
+        {
+            return NotFound($"Aluno com o id {id} não encontrado.");
+        }
+        return Ok(aluno);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Aluno aluno)
+    {
+        aluno.Id = 0;
+        await _alunoService.CreateAluno(aluno);
+        return Ok();
+    }
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> Update(int id, [FromBody] Aluno aluno)
+    {
+        if (id != aluno.Id)
+            return BadRequest("Dados inconsistentes");
+
+        await _alunoService.UpdateAluno(aluno);
+        return Ok($"Aluno id {id} atualizado com sucesso.");
+    }
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        Aluno? aluno = await _alunoService.GetAluno(id);
+        if (aluno == null)
+        {
+            return NotFound($"Aluno com id {id} não encontrado");
+        }
+        await _alunoService.DeleteAluno(aluno);
+        return Ok($"Aluno id {id} excluído com sucesso.");
+    }
+
 }
--- Interfaces/IAlunoService.cs
+++ Interfaces/IAlunoService.cs
@@ -5,4 +5,9 @@ namespace MinicursoAPI.Interfaces;
 public interface IAlunoService
 {
     Task<IEnumerable<Aluno>> GetAlunos();
+    Task CreateAluno(Aluno aluno);
+    Task UpdateAluno(Aluno aluno);
+    Task DeleteAluno(Aluno aluno);
+    Task<Aluno?> GetAluno(int id);
+    Task<IEnumerable<Aluno>> GetAlunosByNome(string nome);
 }
